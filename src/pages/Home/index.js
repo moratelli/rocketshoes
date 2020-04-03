4a0d666aaf5ee433830d4494ecb749248dacad10
Pedro <pedromoratelli@gmail.com>
2020-04-03 02:09:38 -0300

    Style Home Component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,107 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-//  import {Container} from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-air-zoom-structure-22-masculino/58/HZM-0438-158/HZM-0438-158_zoom1.jpg?ts=1582301919&ims=326x"
+          alt="Nike Air Zoom Structure 22"
+        />
+        <strong>Nike Air Zoom Structure 22</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-air-zoom-structure-22-masculino/58/HZM-0438-158/HZM-0438-158_zoom1.jpg?ts=1582301919&ims=326x"
+          alt="Nike Air Zoom Structure 22"
+        />
+        <strong>Nike Air Zoom Structure 22</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-air-zoom-structure-22-masculino/58/HZM-0438-158/HZM-0438-158_zoom1.jpg?ts=1582301919&ims=326x"
+          alt="Nike Air Zoom Structure 22"
+        />
+        <strong>Nike Air Zoom Structure 22</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-air-zoom-structure-22-masculino/58/HZM-0438-158/HZM-0438-158_zoom1.jpg?ts=1582301919&ims=326x"
+          alt="Nike Air Zoom Structure 22"
+        />
+        <strong>Nike Air Zoom Structure 22</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-air-zoom-structure-22-masculino/58/HZM-0438-158/HZM-0438-158_zoom1.jpg?ts=1582301919&ims=326x"
+          alt="Nike Air Zoom Structure 22"
+        />
+        <strong>Nike Air Zoom Structure 22</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-air-zoom-structure-22-masculino/58/HZM-0438-158/HZM-0438-158_zoom1.jpg?ts=1582301919&ims=326x"
+          alt="Nike Air Zoom Structure 22"
+        />
+        <strong>Nike Air Zoom Structure 22</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
