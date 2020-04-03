e73ad6ef5f54e237e1f77bb18810e6d185a8c03c
Pedro <pedromoratelli@gmail.com>
2020-04-03 02:44:42 -0300

    Style Cart Component

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,68 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-//  import {Container} from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUCT</th>
+            <th>QTY</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-nike-air-zoom-structure-22-masculino/58/HZM-0438-158/HZM-0438-158_zoom1.jpg?ts=1582301919&ims=326x"
+                alt="Nike Air Zoom Structure 22"
+              />
+            </td>
+            <td>
+              <strong>Nike Air Zoom Structure 22</strong>
+              <span>R$129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Go to Checkout</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$420,69</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
